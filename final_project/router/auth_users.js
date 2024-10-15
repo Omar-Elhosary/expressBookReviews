595bc9d70cbb52e35c14cc992721869baa8e9244
Omar-Elhosary <omarzaky646@gmail.com>
2024-10-16 01:28:27 +0300

    User Login Done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,55 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ // returns boolean
-// write code to check is the username is valid
-}
+const isValid = (username) => {
+  const user = users.filter((user) => user.username == username);
+  return user.length == 0;
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  const user = users.filter(
+    (user) => user.username == username && user.password == password
+  );
+  return user.length > 0;
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!(username && password)) {
+    return res
+      .status(400)
+      .json({ message: "Username or Password aren't valid" });
+  }
+  if (authenticatedUser(username, password)) {
+    const token = jwt.sign({ username }, "fingerprint_customer", {
+      expiresIn: "15s",
+    });
+    req.session.token = token;
+    req.session.username = username;
+
+    return res.send("Congrats, You logged in successfully!");
+  }
+  return res
+    .status(403)
+    .json({ message: "Username or Password aren't correct" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const username = req.session.username;
+  const token = req.session.token;
+  const review = req.query;
+  if (books[isbn]) {
+    const book = books[isbn];
+    book.reviews[username] = review;
+  }
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
