e8f5afee72e820e354ee26f428ee25682b72232d
Omar-Elhosary <omarzaky646@gmail.com>
2024-10-15 23:18:17 +0300

    View Book With Title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,8 +41,14 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const book = Object.values(books).filter((book) => book.title == title);
+  console.log(book);
+
+  if (book.length > 0) {
+    return res.status(200).json(book[0]);
+  }
+  return res.status(400).json({ message: "This book isn't found" });
 });
 
 //  Get book review
