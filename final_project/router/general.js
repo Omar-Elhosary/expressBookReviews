aefba7ae61db10eeed386a36846d7078883818c6
Omar-Elhosary <omarzaky646@gmail.com>
2024-10-15 23:13:15 +0300

    View Author's Books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,16 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const authorBooks = Object.values(books).filter(book => book.author == author);
+  console.log(authorBooks);
+  
+  if (authorBooks.length > 0) {
+    return res.status(200).json(authorBooks);
+  }
+  return res
+    .status(400)
+    .json({ message: "No books're found for this author" });
 });
 
 // Get all books based on title
