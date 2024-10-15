acd4b0bfe7702a143a9d577c80eaae8f0dd27e78
Omar-Elhosary <omarzaky646@gmail.com>
2024-10-16 01:11:37 +0300

    User Registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,12 +1,32 @@
 const express = require("express");
 let books = require("./booksdb.js");
+const session = require("express-session");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!(username && password)) {
+    return res
+      .status(400)
+      .json({ message: "Username or Password aren't valid" });
+  }
+
+  if (!isValid(username)) {
+    return res
+      .status(409)
+      .json({ message: username + "is already existed. Login instead" });
+  }
+
+  users.push({
+    username,
+    password,
+  });
+  
+  return res.send(username + " has been created successfully!!");
 });
 
 // Get the book list available in the shop
@@ -28,15 +48,15 @@ public_users.get("/isbn/:isbn", function (req, res) {
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   const author = req.params.author;
-  const authorBooks = Object.values(books).filter(book => book.author == author);
+  const authorBooks = Object.values(books).filter(
+    (book) => book.author == author
+  );
   console.log(authorBooks);
-  
+
   if (authorBooks.length > 0) {
     return res.status(200).json(authorBooks);
   }
-  return res
-    .status(400)
-    .json({ message: "No books're found for this author" });
+  return res.status(400).json({ message: "No books're found for this author" });
 });
 
 // Get all books based on title
@@ -53,8 +73,13 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn].reviews);
+  }
+  return res
+    .status(400)
+    .json({ message: "ISBN is invalid or The book isn't found" });
 });
 
 module.exports.general = public_users;
